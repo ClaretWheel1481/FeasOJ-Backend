17d4c06aa0315552d811d1636c622b5f2e637325
ClaretWheel1481 <LanceHuang245@outlook.com>
2025-01-11 01:46:08 +0800

    fix: 修复反向代理导致SSE错误

--- src/internal/gin/sse.go
+++ src/internal/gin/sse.go
@@ -2,6 +2,7 @@ package gincontext
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,10 +21,16 @@ func SSEHandler(c *gin.Context) {
 	c.Writer.Header().Set("Connection", "keep-alive")
 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 
+	ticker := time.NewTicker(30 * time.Second) // 30秒发送一次心跳
+	defer ticker.Stop()
+
 	for {
 		select {
 		case message := <-messageChan:
 			fmt.Fprintf(c.Writer, "data: %s\n\n", message)
+			c.Writer.Flush()
+		case <-ticker.C:
+			fmt.Fprintf(c.Writer, ": keep-alive\n\n")
 			c.Writer.Flush()
 		case <-c.Request.Context().Done():
 			return
