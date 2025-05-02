4aa7f08bf1707cfaff7b24c0a44bb65c8da725ec
ClaretWheel1481 <LanceHuang245@outlook.com>
2025-05-02 15:06:25 +0800

    fix: 修复当题目不属于任何竞赛时，无法查看提交状态Bug

--- internal/utils/sql/submit_records.go
+++ internal/utils/sql/submit_records.go
@@ -18,7 +18,6 @@ func SelectSubmitRecordsByUid(uid int) []global.SubmitRecord {
 // 如果题目所属竞赛正在进行中，则返回的 Code 字段为空字符串
 func SelectSRByUidForChecker(uid int) []global.SubmitRecord {
 	var records []global.SubmitRecord
-
 	selectFields := []string{
 		"submit_records.sid",
 		"submit_records.pid",
@@ -47,26 +46,25 @@ func SelectSRByUidForChecker(uid int) []global.SubmitRecord {
 // 返回 SubmitRecord 表中 30 天内的记录
 func SelectAllSubmitRecords() []global.SubmitRecord {
 	var records []global.SubmitRecord
-
 	selectFields := []string{
-		"submit_records.sid",
-		"submit_records.pid",
-		"submit_records.uid",
-		"submit_records.username",
-		"submit_records.result",
-		"submit_records.time",
-		"submit_records.language",
-		// 如果竞赛正在进行(status=1)，则返回空字符串，否则返回原 code
-		"CASE WHEN competitions.status = 1 THEN '' ELSE submit_records.code END AS code",
+		"sr.sid",
+		"sr.pid",
+		"sr.uid",
+		"sr.username",
+		"sr.result",
+		"sr.time",
+		"sr.language",
+		"CASE WHEN c.status = 1 THEN '' ELSE sr.code END AS code",
 	}
 
 	global.DB.
-		Table("submit_records").
+		Table("submit_records AS sr").
 		Select(strings.Join(selectFields, ", ")).
-		Joins("JOIN problems ON problems.pid = submit_records.pid").
-		Joins("JOIN competitions ON competitions.contest_id = problems.contest_id").
-		Where("submit_records.time > ?", time.Now().Add(-30*24*time.Hour)).
-		Order("submit_records.time DESC").
+		Joins("JOIN problems AS p ON p.pid = sr.pid").
+		Joins("LEFT JOIN competitions AS c ON c.contest_id = p.contest_id").
+		Where("sr.time > ?", time.Now().Add(-30*24*time.Hour)).
+		Where("c.status IS NULL OR c.status != 0 OR p.is_visible = ?", true).
+		Order("sr.time DESC").
 		Find(&records)
 
 	return records
